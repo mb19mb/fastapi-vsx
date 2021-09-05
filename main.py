999cd75160ab33e23ce10f9e80a86c14d53c6ddc
mb19mb <89995526+mb19mb@users.noreply.github.com>
2021-09-05 22:10:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def vsx_volume( vol: Volume):
     print(vol)
     v = imp.load_source("habridge.vsx", "/home/pi/habridge/skripte/vsx.py")
     vsx = v.VSX()
-    vsx.volume(vol.raw)
-    return {"item_id": "2"}
+    vsx.volume(vol.percent)
+    return {}
 
 @app.get("/vsx/on")
 def vsx_on():
