afc21b650a352c316294364daf7181d04308d17e
mb19mb <89995526+mb19mb@users.noreply.github.com>
2021-09-05 22:05:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from pydantic import BaseModel
 import subprocess, imp
 
 app = FastAPI()
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
         
 class Volume(BaseModel):
     raw: int
@@ -18,15 +13,6 @@ class Volume(BaseModel):
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.post("/items")
-def update_item( item: Item):
-    print(item)
-    return {"item_name": item.name, "item_id": "2"}
 
 @app.post("/vsx/volume")
 def vsx_volume( vol: Volume):
