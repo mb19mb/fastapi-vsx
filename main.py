45db3acf36dd4f23391f39365e93731285e631f5
mb19mb <89995526+mb19mb@users.noreply.github.com>
2021-09-05 21:42:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Optional[bool] = None
+        
+class Volume(BaseModel):
+    raw: int
+    percent: int
+        
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
@@ -23,6 +27,14 @@ def read_item(item_id: int, q: Optional[str] = None):
 def update_item( item: Item):
     print(item)
     return {"item_name": item.name, "item_id": "2"}
+
+@app.post("/vsx/volume")
+def vsx_volume( vol: Volume):
+    print(vol)
+    v = imp.load_source("habridge.vsx", "/home/pi/habridge/skripte/vsx.py")
+    vsx = v.VSX()
+    vsx.volume(vol.raw)
+    return {"item_id": "2"}
 
 @app.get("/vsx/on")
 def vsx_on():
