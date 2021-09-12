cb2a139f2a3a724886ccef1f4980a714577a5ed2
Matthias Beyer <m.beyer@axilaris.de>
2021-09-12 20:30:04 +0200

    t

--- Vsx.py
+++ Vsx.py
@@ -3,12 +3,9 @@ from datetime import datetime
 from VsxTelnetClient import VsxTelnetClient
 import sys, subprocess, imp
 
-
 # noinspection PyInconsistentIndentation
 class Vsx:
-    path = "/home/pi/habridge/skripte/"
     logEnabled = True
-    volumeTmpFile = ""
     vMax = 131
     vCurrent = 92
     vUpStepSize = 15
@@ -19,26 +16,10 @@ class Vsx:
         self.__readConfigParams()
         self.vsxTelnetClient = VsxTelnetClient()
 
-    def __readConfigParams(self):
-        f = open(self.path + "config.sh", "r")
-        for line in f:
-            if line.startswith("VSX_VOL_TMP_FILE"):
-                self.volumeTmpFile = line.split("=")[1]
-            if line.startswith("MAXVOL"):
-                self.vMax = line.split("=")[1]
-        f.close()
-
     def __readCurrentVolume(self):
-        # Aktuelle Lautstaerke ermitteln und in eine Dateischreiben
-        subprocess.call(self.path + "lautstaerkestatus.sh")
-
-        f = open(self.volumeTmpFile, "r")
-        for line in f:
-            if line.startswith("VOL"):
-                self.vCurrent = int(line.lstrip("VOL").strip())
-                break
-
-        self.__vCurrentIsNumeric()
+        self.vsxTelnetClient.command("?V")
+        self.vCurrent = int(self.vsxTelnetClient.getLastCommandResult().replace("VOL", ""))
+        self.__checkCurrentIsNumeric()
         self.__log("aktuelle Lautstaerke: " + str(self.vCurrent))
 
     def volume(self, percent):
@@ -84,27 +65,10 @@ class Vsx:
         self.__log("PowerOn senden")
         self.vsxTelnetClient.command("PO") # PowerOn
 
-    def lauter2(self):
-        self.__log("Lauter")
-        # ermittelte Lautstaerke einlesen
-        self.__readCurrentVolume()
-
-        # Neue Lautstaerke setzen
-        vnew = self.vCurrent + self.vUpStepSize
-
-        # MAXWert ueberschritten? Beende
-        if int(vnew) > int(self.vMax):
-            self.__log("zulaut... mache nix")
-            sys.exit()
-
-        vnew = (str(vnew) + "VL").rjust(5, "0")
-        self.__log("neuer Lautstaerkewerte: " + vnew + "\n")
-        subprocess.call([self.path + "vsxExeCmd.sh", str(vnew)])
 
     def lauter(self):
-        self.__log("Lauter")
-        self.vsxTelnetClient.command("?V")
-        self.vCurrent = int(self.vsxTelnetClient.getLastCommandResult().replace("VOL", ""))
+        self.__log("lauter()")
+        self.__readCurrentVolume()
 
         vnew = self.vCurrent + self.vUpStepSize # Neue Lautstaerke setzen
 
@@ -118,17 +82,16 @@ class Vsx:
         self.vsxTelnetClient.command(str(vnew))
 
 
-    def __vCurrentIsNumeric(self):
+    def __checkCurrentIsNumeric(self):
         # Konnte Lautstaerke nicht ermittelt werden, setzen wir Volume auf 92
         if str(self.vCurrent).isnumeric() == False:
             self.__log("vCurrent is not numceric. Adapt vCurrent...")
             self.vCurrent = 92
 
     def leiser(self):
-        self.__log("leiser")
+        self.__log("leiser()")
 
-        self.vsxTelnetClient.command("?V")
-        self.vCurrent = int(self.vsxTelnetClient.getLastCommandResult().replace("VOL", ""))
+        self.__readCurrentVolume()
 
         vnew = self.vCurrent - self.vUpStepSize
         vnew = (str(vnew) + "VL").rjust(5, "0")
